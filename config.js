7d7945cb0d4d337cc1a18c00fdcd9c64367b101e
Quentin Li <quentinl@bu.edu>
2015-04-23 01:23:28 -0400

    preliminary models

---
+++ config.js
@@ -0,0 +1,10 @@
+var db = {
+    host : "localhost",
+    name : "quentinl",
+    username : "quentinl",
+    password : "Qw3rty",
+    env : {
+        dialect : "postgres",
+        port : 5432
+    }
+}
