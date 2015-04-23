889ca53e894b3c5957d0efaabcef7f22650ef374
Huy Le <huyle333@yahoo.com>
2015-04-23 18:41:51 -0400

    changed User.js with bug fixes

--- models/User.js
+++ models/User.js
@@ -2,41 +2,41 @@
 var db = require('../models/index');
 
 function User(data) {
-    this.username = data.username;
-    this.password = data.password;
     this.email = data.email;
+    this.password = data.password;
     this.first_name = data.first_name;
     this.last_name = data.last_name;
     this.dob = data.dob;
     this.gender = data.gender;
     // this.location = data.location;
-    this.user_id = data.id;
+    this.user_id = data.user_id;
 }
 
 User.prototype.getId = function() {
-    db.query('SELECT user_id from Users WHERE username = ', this.username)
-        .then(function(uid) {
-            return uid;
+    db.query('SELECT user_id from Users WHERE email = ', this.email)
+        .then(function(user_id) {
+            return user_id;
         });
 }
 
 User.prototype.addUser = function() {
-    var query = db.query('INSERT INTO Users (username, password, email, first_name, last_name, dob, gender) VALUES (?, ?, ?, ?, ?, to_date(?, \'MM DD YYYY\'), ?)', 
-            {replacements: [this.username, this.password, this.email, this.first_name, this.last_name, this.dob, this.gender ]});
+    var query = db.query('INSERT INTO Users (email, password, first_name, last_name, dob, gender) VALUES (?, ?, ?, ?, to_date(?, \'MM DD YYYY\'), ?)', 
+            {replacements: [this.email, this.password, this.first_name, this.last_name, this.dob, this.gender ], type:'INSERT'});
     return query;
 }
 
 User.getUser = function() {
-    db.query('SELECT username, password, email, fname, lname, dob, gender, location, id from Users WHERE id = ?', {replacements: [this.user_id]})
-        .then(function(uid) {
-            return uid;
+    db.query('SELECT email, password, first_name, last_name, dob, gender, user_id from Users WHERE user_id = ?', {replacements: [this.user_id]})
+        .then(function(user_id) {
+            return user_id;
         });
 }
-User.getUserCredential = function(email, password,callback) {
+
+User.getUserCredential = function(email, password, callback) {
     db.query('SELECT email, password FROM Users WHERE email = ?', 
             {replacements: [email, password]})
     .then(function(response) {
-        callback(response)
+        callback(response);
     }).catch(function(err) {
         callback(new Error(err));
     });
